02b692f5e98289642b13bd35c5552287e4c42b6e
Jack Evans <jackevans24@googlemail.com>
2022-05-12 11:33:02 +0100

    Don't destroy music on load

---
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class MusicManager : MonoBehaviour
+    {
+        private static MusicManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                Destroy(this.gameObject);
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
