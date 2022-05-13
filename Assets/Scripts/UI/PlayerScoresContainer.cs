5d9c2b4845b8f134f42c09af2aa1deda6dc2f48d
Jack Evans <jackevans24@googlemail.com>
2022-05-13 14:51:36 +0100

    Edit scores position on game over

---
+++ Assets/Scripts/UI/PlayerScoresContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class PlayerScoresContainer : MonoBehaviour
+    {
+        [SerializeField] private Vector2 gameOverMinAnchor;
+        [SerializeField] private Vector2 gameOverMaxAnchor;
+
+        private RectTransform rectTransform;
+
+        private void Awake()
+        {
+            this.rectTransform = GetComponent<RectTransform>();
+        }
+
+        public void GameOver()
+        {
+            rectTransform.anchorMin = gameOverMinAnchor;
+            rectTransform.anchorMax = gameOverMaxAnchor;
+        }
+    }
+}
